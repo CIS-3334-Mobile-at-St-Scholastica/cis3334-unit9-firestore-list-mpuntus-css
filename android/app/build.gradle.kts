1f17866c9ad44cd8c93d6fec48e58b3480e79460
mpuntus-css <mpuntus@css.edu>
2025-11-09 23:05:05 -0600

    Completed Version of the App

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -3,8 +3,19 @@ plugins {
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+    // Add the Google services Gradle plugin
+    id("com.google.gms.google-services")
 }
 
+dependencies {
+    // Import the Firebase BoM
+    implementation(platform("com.google.firebase:firebase-bom:34.5.0"))
+
+
+    // TODO: Add the dependencies for Firebase products you want to use
+    // When using the BoM, don't specify versions in Firebase dependencies
+    // https://firebase.google.com/docs/android/setup#available-libraries
+}
 android {
     namespace = "css.cis3334.flutter_firestore_itemlist"
     compileSdk = flutter.compileSdkVersion
@@ -29,6 +40,10 @@ android {
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
+
+//    buildFeatures {
+//        buildConfig = true
+//    }
 
     buildTypes {
         release {
